60bc286d81083c340098b6506008db255d6cbdc2
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2024-07-24 23:16:55 -0300

    comitum

---
+++ salario.py
@@ -0,0 +1,9 @@
+salario = float(input('Coloque seu salário: '))
+
+conta = salario / 30 
+
+horas = float(input('Quantas horas diárias você trabalha? '))
+
+conta1 = conta / horas
+
+print('O valor da sua hora trabalhada é', conta1)
